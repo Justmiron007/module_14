aafb676bf0c12b581701809bb018333428440ebe
Justmiron007 <raen.96@mail.ru>
2024-10-14 01:04:01 +0500

    Add files via upload

---
+++ module_14_1.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER,
+    balance INTEGER NOT NULL
+)
+''')
+for i in range(10):
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+                   (f'User{i}', f'example{i}@gmail.com', f'{i}0', '1000'))
