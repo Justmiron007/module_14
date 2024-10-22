d77f4602dda93b7f9492b90a32ca9c0ad9b8e2d6
Justmiron007 <raen.96@mail.ru>
2024-10-23 01:58:54 +0500

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -1,10 +1,10 @@
 import sqlite3
 
+connection = sqlite3.connect('products.db')
+cursor = connection.cursor()
+
 
 def initiate_db():
-    connection = sqlite3.connect('products.db')
-    cursor = connection.cursor()
-
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Products(
         id INTEGER PRIMARY KEY,
@@ -13,12 +13,21 @@ def initiate_db():
         price INTEGER NOT NULL
     )
     ''')
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        age INTEGER NOT NULL,
+        balance INTEGER NOT NULL
+    )
+    ''')
 
     for i in range(1, 5):
         cursor.execute('REPLACE INTO Products (id, title, description, price) VALUES (?, ?, ?, ?)',
-                       (f'{i}', f'Продукт {i}', f'Описание {i}', f'{i*100}'))
+                       (f'{i}', f'Продукт {i}', f'Описание {i}', f'{i * 100}'))
     connection.commit()
-    connection.close()
+    # connection.close()
 
 
 def get_all_products():
@@ -29,3 +38,18 @@ def get_all_products():
     connection.commit()
     return products
 
+
+def add_user(username, email, age, balance=1000):
+    cursor.execute(
+        f"REPLACE INTO Users (username, email, age, balance) VALUES ('{username}', '{email}', {age}, {balance})")
+    connection.commit()
+
+
+def is_included(username):
+    query = "SELECT * FROM Users WHERE username = ?"
+    cursor.execute(query, (username,))
+    user = cursor.fetchone()
+    if user:
+        return True
+    else:
+        return False
